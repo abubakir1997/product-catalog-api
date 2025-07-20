a4f7d995f12477204005625efff0abb70c703668
Abdelrahman Salem <abubakir1997@gmail.com>
2025-07-20 14:23:27 -0400

    return the updated document using the new option

--- src/api/updateProduct.ts
+++ src/api/updateProduct.ts
@@ -2,5 +2,5 @@ import { ProductModel } from '@/schema/product'
 import { Product } from '@/types/Product'
 
 export async function updateProduct(id: string, product: Partial<Product>): Promise<Product | null> {
-  return ProductModel.findOneAndUpdate({ _id: id }, product)
+  return ProductModel.findByIdAndUpdate({ _id: id }, product, { new: true })
 }
